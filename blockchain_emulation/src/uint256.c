599ed40b000e5f47733cbcff6d35ea43bec9ac2e
Tomas Kanocz <tomas.kanocz@cnl.sk>
2017-12-18 23:07:12 +0100

    worked on str_to_uint256_t - getting core dumped, not finished!

--- blockchain_emulation/src/uint256.c
+++ blockchain_emulation/src/uint256.c
@@ -1,7 +1,8 @@
-#include <uint256.h>
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <uint256.h>
 
 
 bool validate_uint256_string(char* s, size_t str_len){
@@ -10,8 +11,8 @@ bool validate_uint256_string(char* s, size_t str_len){
 
 	if(s[0] == '0' && s[1] == 'x') count = 2;
 
-	if(count == 0 && str_len != 64) return false;
-	else if(count == 2 && str_len != 66) return false;
+	if(count == 0 && str_len != UINT256T_LENGTH) return false;
+	else if(count == 2 && str_len != UINT256T_LENGTH+2) return false;
 
 	if(count ==2) printf("there is 0x at the beginning\n");
 
@@ -26,7 +27,24 @@ bool validate_uint256_string(char* s, size_t str_len){
 void str_to_uint256_t(char* string, uint256_t* ptr, size_t str_len){
 	if(!validate_uint256_string(string, str_len)){
 		printf("Error: string %s is not valid hex format!\n", string);
-		//exit(EXIT_FAILURE);
 	}
-	else printf("all is ok!\n");
+	size_t count = 0;
+	char* endptr = NULL;
+	char* partial_string = NULL;
+	
+	while(1){
+		strcpy(partial_string, (string+count*( UINT256T_LENGTH / 32), (size_t)UINT256T_LENGTH/32));	
+		ptr->data[count++] = (uint32_t) strtol(partial_string, &endptr, 16);
+		if(count == UINT256T_LENGTH/32) break;
+	}
+	
+	puts("the original string is:");
+	puts(string);
+	puts("the hopefully working one is:");
+
+	for(count = 0; count < UINT256T_LENGTH/32; count++){
+		printf("%2.0X", ptr->data[count]);
+	}
+	printf("\n");
+	
 }
